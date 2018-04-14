0e5ba85ea17755972bab6952e71c78b29dc26d28
Oleg Malaphey <oleg.malaphey@gmail.com>
2018-04-14 16:17:25 -0700

    Restart service on config changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,6 +40,7 @@ class atop (
     group   => $atop::params::conf_file_group,
     mode    => $atop::params::conf_file_mode,
     content => template($atop::params::conf_file_template),
+    notify  => Service[$service_name],
   } ->
   service { $service_name:
     ensure => $service_state,
